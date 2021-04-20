46a58bb6b888ea257294fb220e24df73b41dac75
Purushotam-Cogent <purushotam.sinha@cogentinfo.com>
2021-04-20 15:10:50 -0400

    add all files

---
+++ src/test/java/com/porus/test/RestAPITest/RestApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porus.test.RestAPITest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
